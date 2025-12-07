dec135adbd435dd537f4c61cfffdebcbdf2e7329
Laura <lauraschlueter@ymail.com>
2025-12-07 15:34:26 -0800

    (chore)Cleanup: dead code

--- client/server/routes/api.js
+++ client/server/routes/api.js
@@ -3,30 +3,26 @@ import recipeController from '../controllers/recipeController.js';
 
 const router = express.Router();
 
-/* route to get recipes*/
 router.get('/recipes',
     recipeController.getRecipes,
     (req, res) => res.status(200).json(res.locals.recipes)
 );
 
-/* route to view recipe details */
 router.get('/recipes/:id',
     recipeController.getRecipeDetails,
     (req, res) => res.status(200).json(res.locals.recipeDetails)
 );
 
-/* route to save recipes */
 router.post('/recipe', 
     recipeController.saveRecipe,
     (req, res) => res.status(200).json(res.locals.saved)
 );
 
-/* route to get all saved recipes */
-    /* I THINK THIS MIGHT GO SOMEWHERE ELSE */
 router.get('/saved', 
     recipeController.getSavedRecipes,
     (req, res) => res.status(200).json(res.locals.savedRecipes)
 );
-/* route to delete recipes */
+
+/* stretch feature: route to remove recipes */
 
 export default router;
--- client/src/RecipeDetails.jsx
+++ client/src/RecipeDetails.jsx
@@ -1,76 +1,74 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import parse from 'html-react-parser';
-import { useNavigate } from 'react-router-dom';
-
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import parse from "html-react-parser";
+import { useNavigate } from "react-router-dom";
 
 const RecipeDetails = () => {
-    const navigate = useNavigate();
-// have destructuring inside my component
-    const [ viewfullRecipe, setViewFullRecipe ] = useState(null);
-// to access different recipe id's from my route
-    const { id } = useParams();
-// i need to use useEffect so that this page loads
-//  when my routes change  
-    useEffect(() => {
-        // this is where I will have my working logic
-        const fetchFullRecipeDetails = async () => {
-            try {
-                const res = await fetch(`/api/recipes/${id}`);
-                const fullRecipeDetails = await res.json();
-                console.log('✅ Fetched full recipe details', fullRecipeDetails);
-                setViewFullRecipe(fullRecipeDetails);
-            } catch (err) {
-              console.error('❌ Error fetching full recipes:', err);  
-            }
-        };
-        fetchFullRecipeDetails();
-        // add an id dependency array for useEffect
-    }, [id]);
+  const navigate = useNavigate();
+  const [viewfullRecipe, setViewFullRecipe] = useState(null);
+  const { id } = useParams();
 
-    const handleSaveRecipe = async () => {
-        try {
-            const recipeToSave = {
-                id: viewfullRecipe.id,
-                title: viewfullRecipe.title,
-                ingredients: viewfullRecipe.ingredients,
-            };
-            console.log('✅ Recipe being saved:', recipeToSave);
-            const res = await fetch('/api/recipe', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json'},
-                body: JSON.stringify(recipeToSave),
-            });
-            const savedData = await res.json();
-            console.log('✅ Recipe saved:', savedData);
-        } catch (err) {
-            console.error('❌ Error saving recipe:', err);
-        }
+  useEffect(() => {
+    const fetchFullRecipeDetails = async () => {
+      try {
+        const res = await fetch(`/api/recipes/${id}`);
+        const fullRecipeDetails = await res.json();
+        console.log("✅ Fetched full recipe details", fullRecipeDetails);
+        setViewFullRecipe(fullRecipeDetails);
+      } catch (err) {
+        console.error("❌ Error fetching full recipes:", err);
+      }
     };
-// in this return I want to try using the terinary operator
-    return(
-        <>
-            <button className="home-button" onClick={() => navigate('/')}>Home</button>
-        <div className='full-recipe-container'>
-            {viewfullRecipe ? (
-                <>
-                <h2>{viewfullRecipe.title}</h2>
-                <img src={viewfullRecipe.image} alt={viewfullRecipe.title} />
-                <div className='FR-button'>
-                  <button onClick={handleSaveRecipe}>Save Recipe</button>
-                </div>
-                {parse(viewfullRecipe.instructions)}
-                <ul>
-                    {viewfullRecipe.ingredients.map((item, index) =>
-                    <li key={index}>{item}</li>)}
-                </ul>
-                </>
-            ) : (
-                <p>🍽️ loading full recipe 🍽️</p>
-            )}
-        </div>
-        </>
-    );
+    fetchFullRecipeDetails();
+    // add an id dependency array for useEffect
+  }, [id]);
+
+  const handleSaveRecipe = async () => {
+    try {
+      const recipeToSave = {
+        id: viewfullRecipe.id,
+        title: viewfullRecipe.title,
+        ingredients: viewfullRecipe.ingredients,
+      };
+      console.log("✅ Recipe being saved:", recipeToSave);
+      const res = await fetch("/api/recipe", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(recipeToSave),
+      });
+      const savedData = await res.json();
+      console.log("✅ Recipe saved:", savedData);
+    } catch (err) {
+      console.error("❌ Error saving recipe:", err);
+    }
+  };
+  // in this return I want to try using the terinary operator
+  return (
+    <>
+      <button className="home-button" onClick={() => navigate("/")}>
+        Home
+      </button>
+      <div className="full-recipe-container">
+        {viewfullRecipe ? (
+          <>
+            <h2>{viewfullRecipe.title}</h2>
+            <img src={viewfullRecipe.image} alt={viewfullRecipe.title} />
+            <div className="FR-button">
+              <button onClick={handleSaveRecipe}>Save Recipe</button>
+            </div>
+            {parse(viewfullRecipe.instructions)}
+            <ul>
+              {viewfullRecipe.ingredients.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          </>
+        ) : (
+          <p>🍽️ loading full recipe 🍽️</p>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default RecipeDetails;
