f780aaea584f532dcc7315557c6138b0993da85e
Laura <lauraschlueter@ymail.com>
2026-03-02 16:26:27 -0800

    (chore): erase dead code

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,12 +1,11 @@
-import express from 'express';
-import { signup, login, getMe } from '../controllers/authController.js';
-import { protect } from '../middleware/authMiddleware.js'
+import express from "express";
+import { signup, login, getMe } from "../controllers/authController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post('/signup', signup);
-router.post('/login', login);
-router.get('/me', protect, getMe);
+router.post("/signup", signup);
+router.post("/login", login);
+router.get("/me", protect, getMe);
 
 export default router;
-
